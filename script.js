71d4cf3b9fae4b19dbaa49661a21fb7817f1928a
Dominik015 <158041727+Dominik015@users.noreply.github.com>
2026-02-19 14:09:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,17 @@ function AddTable() {
     let kiemel = document.getElementById("highlight").checked;
     for (let i = 0; i < sorok; i++) {
         let tr = document.createElement("tr");
-
         for (let c = 0; c < oszlopok; c++) {
+                let cella;
+                if (kiemel && (i === 0 || c === 0)) 
+                { 
+                    cella = document.createElement("th"); 
 
-            let cella = (kiemel && (i === 0 || c === 0))
-                ? document.createElement("th")
-                : document.createElement("td");
-
+                } 
+                else 
+                { 
+                    cella = document.createElement("td"); 
+                }
             cella.style.border = borderSzel + "px solid black";
             cella.innerText = Math.round(Math.random() * 9000 + 1000);
 
@@ -33,8 +37,8 @@ function AddTable() {
     if (collapse) {
         table.style.borderCollapse = "collapse";
     }
-
     let container = document.getElementById("container");
     container.appendChild(table);
     container.appendChild(document.createElement("hr"));
 }
+
